572610f6222799a0393c39b31d1aa11935df9c1e
feLycks <felix.obiero@student.moringaschool.com>
2022-01-07 23:12:55 +0300

    push edit deploy build

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/IP-Quote-genrator'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/ip-quote-genrator/index.html'));
+        '/dist/IP-Quote-genrator/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
