2524aa7bd0e6170bdf2fbd5412d2ebbd902ac09f
feLycks <felix.obiero@student.moringaschool.com>
2022-01-07 23:09:08 +0300

    edit deploy build v3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ip-quote-genrator'));
+app.use(express.static(__dirname + '/dist/IP-Quote-genrator'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
         '/dist/ip-quote-genrator/index.html'));
