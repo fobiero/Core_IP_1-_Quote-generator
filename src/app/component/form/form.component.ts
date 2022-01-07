c17b90695de20f9155cfef8df4633dc47f5eeba5
feLycks <felix.obiero@student.moringaschool.com>
2022-01-07 20:01:45 +0300

    Logic build getting started

--- src/app/component/form/form.component.ts
+++ src/app/component/form/form.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 
 @Component({
-  selector: 'app-form',
+  selector: 'form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FormComponent {
+  log(x: any) {console.log(x);}
 }
